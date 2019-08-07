b81f656a1a3d6827130a43873d98321f5c6ba7bc
Natalie Cooper <ncooper@tcd.ie>
2019-08-07 18:42:12 +0100

    Remove unused functions

--- functions/rarefy-functions.R
+++ functions/rarefy-functions.R
@@ -33,23 +33,6 @@ return(pc_data)
 }
 
 #---------------------------------------------------------
-# Select just the number of porpoises needed
-#---------------------------------------------------------
-select_porpoise <- function(nporpoise, points, 
-                            porpoise_list){
-
-# Work out how many porpoises need to be removed
-nremove <- 18 - nporpoise
-# Select a random set of that many porpoises to
-# remove
-exclude <- sample(porpoise_list, nremove)
-
-# Exclude the specimens from ptsarray and continue
-ptsarray[, , -exclude]
-
-}
-
-#---------------------------------------------------------
 # Run MANOVAs
 # To determine if variation within Phocoena phocoena
 # is significantly less than within the clade
@@ -80,37 +63,4 @@ create_MANOVA_output <- function(sims){
   output <- data.frame(array(dim = c(sims, 6)))
   names(output) <- c("nporpoise", "df1", "df2", "Pillai", "approxF", "p")
   return(output)
-}
-
-#----------------------------------------------------------
-# Create ANOVA output file
-#----------------------------------------------------------
-create_ANOVA_output <- function(sims){
-  ANOVA_output <- data.frame(array(dim = c(sims*10, 6)))
-  names(ANOVA_output) <- c("nporpoise", "PC", "df1", "df2", "F", "p")
-  return(ANOVA_output)
-}
-
-#--------------------------------------------
-# Fit ANOVAs for each individual PC
-#--------------------------------------------
-fit_anova <- function(pca, ANOVA_output, nporpoise){
-
-  # List PCs for ANOVA (first 10)
-  pc_list <- names(pca)[16:25]
-  
-  # Run ANOVAs
-  for (j in seq_along(pc_list)){
-    pc <- pc_list[j]
-    x <- fit.anova(pc, pca, "group")
-    ANOVA_output[j,"nporpoise"] <- nporpoise
-    ANOVA_output[j, "PC"] <- pc_list[j]
-    ANOVA_output[j, "df1"] <- x$df[1]
-    ANOVA_output[j, "df2"] <- x$df[2]
-    ANOVA_output[j, "F"] <- x$statistic[1]
-    ANOVA_output[j, "p"] <- x$p.value[1]
-  }
-  
-  return(ANOVA_output)
-  
 }
