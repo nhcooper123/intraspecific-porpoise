e968ca6af5f4b00080dd67ab567d033b38d0cf97
Natalie Cooper <ncooper@tcd.ie>
2019-08-12 14:54:41 +0100

    Finishup plot code

--- analyses/09-rarefaction-plots.R
+++ analyses/09-rarefaction-plots.R
@@ -14,30 +14,37 @@ man <- read_csv(here("outputs/rarefied-MANOVA-results-landmarks.csv"))
 # Extract values for 18 porpoises
 man1 <- read_csv(here("outputs/MANOVA-results-landmarks.csv"))
 
-# Get mean values for p values for each nporpoise
-# and each PC in ANOVAs
-mean_man <-
+# Get median values for p values for each nporpoise
+median_man <-
   man %>%
   filter(!is.na(nporpoise)) %>%
   group_by(nporpoise) %>%
   summarise(mean = median(p),
-            se = sqrt(var(p/length(p))))
-
+            se = sqrt(var(p/length(p))),
+            sig = sum(p < 0.05)/n() * 100)
 # Plots 
 plot1 <- 
-  ggplot(mean_man, aes(x = nporpoise, y = mean)) +
+  ggplot(median_man, aes(x = nporpoise, y = mean)) +
   geom_point(size = 2) +
   geom_errorbar(aes(ymin = mean - se, ymax = mean + se), width = 0.1) +
   theme_bw(base_size = 14) +
-  ylab("mean p value") +
+  ylab("median p value") +
   xlab("number of porpoise specimens") +
-  geom_point(data = man1, aes(x = 18, y = p[1]))
+  geom_point(data = man1, aes(x = 18, y = p[1]), size = 2) +
+  geom_hline(yintercept = 0.05, linetype = 2, colour = "red")
+  
+ggsave(here("outputs/rare-MANOVA-overall.png"), plot1)
 
-ggsave(here("outputs/rare-MANOVA.png"))
-
-
-ggplot(man, aes(x = p, group = nporpoise)) +
+plot2 <- 
+  ggplot(man, aes(x = p)) +
   geom_density() +
   theme_bw(base_size = 14) +
-  xlab("number of porpoise specimens") +
-  facet_wrap(~nporpoise, scales = "free")
+  xlab("MANOVA p value") +
+  facet_wrap(~nporpoise, scales = "free") +
+  theme(strip.background = element_rect(fill = "white"),
+        axis.text.x = element_text(angle = 45, hjust = 1, size = 12)) +
+  geom_vline(data = data.frame(xint = rep(0.05,8), nporpoise = c(2:9)), 
+             aes(xintercept = xint), 
+             linetype = "dashed", colour = "red")
+
+ggsave(here("outputs/rare-MANOVA-p-density.png"), plot2, width = 7, height = 12)
