047fe2f0299bb6f10699fd0e49f052bc34d7a1de
Natalie Cooper <ncooper@tcd.ie>
2019-08-12 15:30:12 +0100

    Add PC3 PC4 plot

--- analyses/06-PCA-plots-linear.R
+++ analyses/06-PCA-plots-linear.R
@@ -33,6 +33,7 @@ hulls_13 <- make_hull(pc_data, pc1 = 16, pc2 = 18, n = 2, grouping_var = 5)
 hulls_23 <- make_hull(pc_data, pc1 = 17, pc2 = 18, n = 2, grouping_var = 5)
 hulls_14 <- make_hull(pc_data, pc1 = 16, pc2 = 19, n = 2, grouping_var = 5)
 hulls_24 <- make_hull(pc_data, pc1 = 17, pc2 = 19, n = 2, grouping_var = 5)
+hulls_34 <- make_hull(pc_data, pc1 = 18, pc2 = 19, n = 2, grouping_var = 5)
 
 
 p1 <-
@@ -110,7 +111,22 @@ p5 <-
   xlab("PC2 (18.65%)") +
   ylab("PC4 (6.355%)")
 
-all <- grid.arrange(p1, p2, p3, p4, p5, nrow = 2)
+p6 <-
+  ggplot(pc_data, aes(x = PC3, y = PC4, col = group)) +
+  geom_point(size = 1, alpha = 0.8) +
+  scale_color_manual(values = c("#008080", "#911eb4")) +
+  theme_bw(base_size = 14) +
+  theme(legend.position = "none") +
+  geom_vline(xintercept = 0, linetype = 2) +
+  geom_hline(yintercept = 0, linetype = 2) +
+  geom_polygon(data = hulls_34[["Phocoena_phocoena"]], aes(x = x, y = y), 
+               col = "#911eb4", fill = "#911eb4", alpha = 0.2) +
+  xlim(-3, 9) +
+  ylim(-5, 5) +
+  xlab("PC3 (8.579%)") +
+  ylab("PC4 (6.355%)")
+
+all <- grid.arrange(p1, p2, p3, p4, p5, p6, nrow = 2)
 
 ggsave(all, filename = here("outputs/PC-polygon-plots-linear.png"))
 #-------------------------------------------
