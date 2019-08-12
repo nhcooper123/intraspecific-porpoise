008aa6629683f396572ffbdac8556ac7c7152ec8
Natalie Cooper <ncooper@tcd.ie>
2019-08-12 12:47:34 +0100

    Remove ANOVAs

--- analyses/09-rarefaction-plots.R
+++ analyses/09-rarefaction-plots.R
@@ -1,4 +1,4 @@
-# Plot results of rearefaction
+# Plot results of rarefaction
 # Aug 2019
 #------------------------------------
 
@@ -10,25 +10,18 @@ library(tidyverse)
 #-------------------------------------
 # Read in data
 man <- read_csv(here("outputs/rarefied-MANOVA-results-landmarks.csv"))
-an <- read_csv(here("outputs/rarefied-ANOVA-results-landmarks.csv"))
 
 # Extract values for 18 porpoises
 man1 <- read_csv(here("outputs/MANOVA-results-landmarks.csv"))
-an1 <- read_csv(here("outputs/ANOVA-results-landmarks.csv"))
 
 # Get mean values for p values for each nporpoise
 # and each PC in ANOVAs
 mean_man <-
   man %>%
+  filter(!is.na(nporpoise)) %>%
   group_by(nporpoise) %>%
-  summarise(mean = mean(p),
-            se = sqrt(var(p/sd(p))))
-
-mean_an <-
-  an %>%
-  group_by(nporpoise, PC) %>%
-  summarise(mean = mean(p),
-            se = sqrt(var(p/sd(p))))
+  summarise(mean = median(p),
+            se = sqrt(var(p/length(p))))
 
 # Plots 
 plot1 <- 
@@ -42,14 +35,9 @@ plot1 <-
 
 ggsave(here("outputs/rare-MANOVA.png"))
 
-plot2 <- 
-  ggplot(mean_an, aes(x = nporpoise, y = mean)) +
-  geom_point(size = 2) +
-  geom_errorbar(aes(ymin = mean - se, ymax = mean + se), width = 0.1) +
-  theme_bw(base_size = 14) +
-  facet_wrap(~PC, ncol = 5) +
-  theme(strip.background = element_rect(fill="white")) +
-  ylab("mean p value") +
-  xlab("number of porpoise specimens")
 
-ggsave(here("outputs/rare_ANOVA.png")) 
+ggplot(man, aes(x = p, group = nporpoise)) +
+  geom_density() +
+  theme_bw(base_size = 14) +
+  xlab("number of porpoise specimens") +
+  facet_wrap(~nporpoise, scales = "free")
