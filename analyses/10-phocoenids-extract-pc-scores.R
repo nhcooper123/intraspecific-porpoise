21288fa6643c3ff99793e63eb61c1423ef807fb9
Natalie Cooper <ncooper@tcd.ie>
2019-11-08 11:15:56 +0000

    Analyses on phocoenids only

---
+++ analyses/10-phocoenids-extract-pc-scores.R
@@ -0,0 +1,107 @@
+# Extract PC scores for just phocoenids
+# Using 3D landmark data
+# Nov 2019
+#------------------------------------
+
+#------------------------------------
+# Load libraries
+#------------------------------------
+library(here)
+library(geomorph)
+library(tidyverse)
+#-------------------------------------
+# Read in metadata
+#-------------------------------------
+ds <- read_csv("raw-data/echo-metadata.csv")
+
+#--------------------------------------------
+# Input landmark data
+# These are in a series of .pts files 
+#--------------------------------------------
+# Define # landmarks and dimensions
+n.landmarks <- 361
+n.dim <- 3
+
+# List .pts files 
+ptslist <- ds %>%
+  pull(filename)
+
+# Create empty array
+ptsarray <- array(dim = c(n.landmarks, n.dim, length(ptslist)))
+
+# Read in files and combine into an array
+for (i in 1:length(ptslist)) {
+  ptsarray[, , i] <-
+    as.matrix(read_table(
+      file = paste0(here("raw-data/landmark-data/"), ptslist[i]),
+      skip = 2,
+      col_names = FALSE,
+      col_types = cols(X1 = col_skip()),
+      n_max = n.landmarks
+    ))
+}
+
+# To ensure everything matches up to metadata
+# add the names of the .pts files to dim names
+dimnames(ptsarray)[[3]] <- ptslist
+
+#---------------------------
+# Select just the phocoenids
+#---------------------------
+ptsarray2 <- ptsarray[, , c(27, 32:53)]
+
+#-----------------------
+# Input additional data
+#-----------------------
+# Semilandmark curves
+slidematrix <- as.matrix(read_csv(here("raw-data/curves.csv")))
+
+#--------------------------------------------------------
+# Generalised Procrustes Analysis
+# to remove position, scale and orientation effects
+#--------------------------------------------------------
+intra_gpa2 <- gpagen(ptsarray2, curves = slidematrix)
+
+# Plot the GPA landmarks if required
+# plot(intra_gpa)
+
+#--------------------------------
+# Principal Components Analysis
+# also creates a plot
+#-------------------------------
+intra_pca2 <- plotTangentSpace(intra_gpa2$coords, legend = TRUE, label = TRUE,
+                              warpgrids = FALSE)
+
+# Look at how PCs explain variance
+# Here we need 26 PCs to explain > 95%
+intra_pca2$pc.summary
+
+# Extract PC scores and make .pts file name into
+# a taxon column for combining with the metadata
+pc_scores2 <- data.frame(filename = rownames(intra_pca2$pc.scores), intra_pca2$pc.scores)
+
+# Merge with metadata
+pc_data2 <- right_join(ds, pc_scores2, by = "filename")
+
+# Write to file
+write_csv(pc_data2, path = here("data/phocoenid-data-landmarks.csv")) 
+
+# Quick plot
+ggplot(pc_data2, aes(PC1, PC2, colour = group)) +
+  geom_point()
+
+#--------------------------------------------
+# Fit Procrustes ANOVAs to coordinates
+#--------------------------------------------
+# Create geomorph dataframe of coordinates and group
+gdf2 <- geomorph.data.frame(intra_gpa2, 
+                           group = pc_data2$group)
+# Fit models
+# randomize raw values
+fit1 <- procD.lm(coords ~ group, 
+                 data = gdf2, iter = 999, 
+                 RRPP = FALSE, print.progress = FALSE) 
+summary(fit1)
+
+# Randomize residuals result will be the same
+# as there is just one explanatory variable.
---
+++ analyses/11-phocoenids-intraspecific-variation.R
@@ -0,0 +1,32 @@
+# Test whether intraspecific variation 
+# in Phocoena phocoena is higher/lower
+# than variation among phocoenids
+# Using 3D landmark data
+# Nov 2019
+#------------------------------------
+
+#------------------------------------
+# Load libraries
+#------------------------------------
+library(here)
+library(geomorph)
+library(tidyverse)
+library(broom)
+library(dispRity)
+
+source("functions/intra-functions.R")
+#--------------------------------------------
+# Input data
+#---------------------------------------------------------
+pc_data <- read_csv(here("data/phocoenid-data-landmarks.csv"))
+
+#---------------------------------------------------------
+# MANOVAs
+# To determine if variation within Phocoena phocoena
+# is significantly less than within the clade
+#----------------------------------------------------------
+# Fit the model for all odontocetes
+# Column 16 = PC1, column 29 = PC14 (95% of variation)
+model1 <- manova(as.matrix(pc_data[, 16:29]) ~ group, data = pc_data)
+# Look at overall model significance
+anova(model1)
