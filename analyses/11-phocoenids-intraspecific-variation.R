1fcf14629af02bc608b5e974bf76980e49ae558b
Natalie Cooper <ncooper@tcd.ie>
2019-11-08 12:02:28 +0000

    Just Phocoena spp.

--- analyses/11-phocoenids-intraspecific-variation.R
+++ analyses/11-phocoenids-intraspecific-variation.R
@@ -18,7 +18,7 @@ source("functions/intra-functions.R")
 #--------------------------------------------
 # Input data
 #---------------------------------------------------------
-pc_data <- read_csv(here("data/phocoenid-data-landmarks.csv"))
+pc_data <- read_csv(here("data/phocoena-data-landmarks.csv"))
 
 #---------------------------------------------------------
 # MANOVAs
@@ -28,5 +28,25 @@ pc_data <- read_csv(here("data/phocoenid-data-landmarks.csv"))
 # Fit the model for all odontocetes
 # Column 16 = PC1, column 29 = PC14 (95% of variation)
 model1 <- manova(as.matrix(pc_data[, 16:29]) ~ group, data = pc_data)
+# Look at overall model significance
+anova(model1)
+
+#--------------------------------------------
+# LINEAR MEASUREMENTS
+#---------------------------------------------
+
+#--------------------------------------------
+# Input data
+#---------------------------------------------------------
+pc_data2 <- read_csv(here("data/phocoena-data-linear.csv"))
+
+#---------------------------------------------------------
+# MANOVAs
+# To determine if variation within Phocoena phocoena
+# is significantly less than within the clade
+#----------------------------------------------------------
+# Fit the model for all odontocetes
+# Column 16 = PC1, column 22 = PC7 (95% of variation)
+model1 <- manova(as.matrix(pc_data2[, 16:22]) ~ group, data = pc_data2)
 # Look at overall model significance
 anova(model1)
